4812000666bd2cb19d52d7bea690b7d65167f362
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2024-04-02 09:29:55 -0300

    update promise

--- pratica/promise.js
+++ pratica/promise.js
@@ -1,5 +1,5 @@
 fetch('http://localhost:3000/produtos')
     .then(res => res.json())
     .then(obj => obj.map(p => p.descricao))
-    .then(listDescricao => console.log(listDescricao))
+    .then(produtosDescricao => console.log(produtosDescricao))
     .catch(err => console.log('Error ao criar lista de descrição', err));
